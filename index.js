4f2caa0ec8d09c52caefdf9553e5affc58d1fa9b
irriz-jayy <jayfelix789@gmail.com>
2022-12-10 12:21:01 +0300

    Done

--- index.js
+++ index.js
@@ -1,2 +1,44 @@
-//Setting base url
-const baseUrl=
+// //Setting base url
+// const baseUrl = "http://localhost:3000/users";
+
+// //Creating form data
+// const data = { name, email };
+// //fetch
+// fetch(baseUrl, {
+//   method: "POST",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify(data),
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     //Appending
+//     document.body.innerHTML = data.id;
+//   })
+//   //Errors
+//   .catch((error) => {
+//     //Appending error message
+//     document.body.innerHTML = error.message;
+//   });
+
+fetch;
+function submitData(name, email) {
+  const data = { name, email };
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      document.body.innerHTML = data.id;
+    })
+    .catch((error) => {
+      document.body.innerHTML = error.message;
+    });
+}
