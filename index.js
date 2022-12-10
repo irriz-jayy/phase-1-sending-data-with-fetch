6272a1f3d1a4224c2e1773431dec60a2c7e96e43
irriz-jayy <jayfelix789@gmail.com>
2022-12-10 11:33:20 +0300

    Changes to source code

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-function submitData(name, email) {
-  const sin = { name, email };
-  return fetch("http://localhost:3000/users", {
-    method: "post",
-    headers: {
-      "Content-Type": "application/json",
-      Accept: "application/json",
-    },
-    body: JSON.stringify(sin),
-  })
-    .then((response) => response.json())
-    .then((data) => {
-      document.body.innerHTML = data.id;
-    })
-    .catch((error) => {
-      document.body.innerHTML = error.message;
-    });
-}
+//Setting base url
+const baseUrl=
